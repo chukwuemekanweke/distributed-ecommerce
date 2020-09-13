11dc5ce098e10a374615a311c6db7a9f8a6a375a
Chukwuemeka Nweke <emekanweke604@gmail.com>
2020-09-13 03:18:59 +0100

    implemented rabbit mq consumer as well as dockerized the ordering API

--- src/Ordering/Ordering.API/Startup.cs
+++ src/Ordering/Ordering.API/Startup.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using AutoMapper;
+using EventBusRabbitMQ;
+using EventBusRabbitMQ.Producer;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -14,12 +16,15 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Ordering.API.Extensions;
+using Ordering.API.RabbitMQ;
 using Ordering.Application.Handlers;
 using Ordering.Core.Repositories;
 using Ordering.Core.Repositories.Base;
 using Ordering.Infrastructure.Data;
 using Ordering.Infrastructure.Repositories;
 using Ordering.Infrastructure.Repositories.Base;
+using RabbitMQ.Client;
 
 namespace Ordering.API
 {
@@ -40,24 +45,56 @@ namespace Ordering.API
             services.AddControllers();
 
 
-            services.AddDbContext<OrderContext>(x => x.UseSqlServer(connectionString));
+            services.AddDbContext<OrderContext>(x => x.UseSqlServer(connectionString), ServiceLifetime.Singleton);
+
+           
+            //required for mediator, that's why we are registering this type of
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+
+            //for mediator
+            services.AddScoped(typeof(IOrderRepository), typeof(OrderRepository));
+            //for rabbit mq
+            services.AddTransient<IOrderRepository, OrderRepository>();
+
 
             services.AddAutoMapper(typeof(Startup));
             services.AddMediatR(typeof(CheckoutOrderHandler).GetTypeInfo().Assembly);
 
-            //for rabbit mq
-            services.AddTransient<IOrderRepository, OrderRepository>();
-
-            //required for mediator, that's why we are registering this type of
-            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
-
-            //for mediator
-            services.AddScoped(typeof(IOrderRepository), typeof(OrderRepository));
 
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Order API", Version = "v1" });
             });
+
+
+            services.AddTransient<EventBusRabbitMQConsumer>();
+            services.AddSingleton<IRabbitMQConnection>(sp => {
+
+                string userName = Configuration["EventBus:UserName"];
+                string password = Configuration["EventBus:Password"];
+
+                var factory = new ConnectionFactory()
+                {
+                    HostName = Configuration["EventBus:HostName"]
+
+                };
+
+                if (!string.IsNullOrWhiteSpace(userName))
+                {
+                    factory.UserName = userName;
+                }
+
+                if (!string.IsNullOrWhiteSpace(password))
+                {
+                    factory.Password = password;
+                }
+
+                return new RabbitMQConnection(factory);
+
+            });
+
+
 
         }
 
@@ -77,6 +114,10 @@ namespace Ordering.API
             {
                 endpoints.MapControllers();
             });
+
+
+            app.UseRabbitListener();
+
 
             app.UseSwagger();
 
